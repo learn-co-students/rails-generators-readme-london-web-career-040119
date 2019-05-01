3695ab1996d611eaf257eef3b523a74dad81cba4
pmhofmann <pm.hofmann@yahoo.co.uk>
2019-05-01 13:46:20 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,7 @@ class PostsController < ApplicationController
   end
 
   def update
+    params.permit!
     @post = Post.find(params[:id])
     @post.update(params.require(:post))
     redirect_to post_path(@post)
